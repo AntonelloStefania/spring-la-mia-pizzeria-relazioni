d35072e55e7352a0dbf5808cbe9adb59f9903b70
AntonelloStefania <antonelloste94@gmail.com>
2023-12-07 15:51:35 +0100

    esercizio completato

--- src/main/java/org/java/spring/controller/DiscountController.java
+++ src/main/java/org/java/spring/controller/DiscountController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -23,16 +24,23 @@ public class DiscountController {
 	@Autowired
 	private DiscountService discountService;
 	
-	@GetMapping("/pizze/discount/create")
-	public String getDiscountForm(Model model) {
-		List<Pizza> pizze = pizzaseService.findAll();
-		model.addAttribute("pizze", pizze);
+	@GetMapping("pizza/{id}/discount/create")
+	public String getDiscountForm(Model model,
+				@PathVariable int id) {
+		Pizza pizza = pizzaseService.findById(id);
+		Discount discount = new Discount();
+		List<Discount> discounts = pizza.getDiscounts();
+	
+		model.addAttribute("pizza",pizza);
+		model.addAttribute("discounts", discounts);
+		model.addAttribute("discount", discount);
 		return "discount-form";
 	}
 	
-	@PostMapping("/pizze/discount/create")
-	public String storeiscount(@ModelAttribute PizzaDiscountDto pizzaDiscountDto) {
-		Pizza pizza = pizzaseService.findById(pizzaDiscountDto.getPizza_id());
+	@PostMapping("/pizza/{id}/discount/create")
+	public String storeDiscount(@ModelAttribute PizzaDiscountDto pizzaDiscountDto,
+			@PathVariable int id) {
+		Pizza pizza = pizzaseService.findById(id);
 		
 		Discount discount = new Discount(pizzaDiscountDto.getName(),
 							pizzaDiscountDto.getStart_date(),
@@ -44,5 +52,21 @@ public class DiscountController {
 		
 		return "redirect:/";
 	}
+	
+	@GetMapping("/pizza/{id}/discount/{discount_id}")
+	public String editDiscount(Model model, @PathVariable int id, @PathVariable int discount_id) {
+		Pizza pizza= pizzaseService.findById(id);
+		Discount discount = discountService.findById(discount_id);
+		
+		model.addAttribute("pizza", pizza);
+		model.addAttribute("discount", discount);
+		
+		return "discount-form";
+	}
+	
+ 
+
+	
+	
 
 }
